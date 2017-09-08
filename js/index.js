bdcf5761affca51706b8fdb7d9d1e6db7e7938e4
EvansShire <Evansshire>
2017-09-08 15:50:40 +0800

    code

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+	
+		$("#birdPlay").click(function(){
+			alert("clicked");
+            audio = document.getElementById('birdAudio');    
+            audio.play();
+                
+                
+                
+    			
+           })
+})
